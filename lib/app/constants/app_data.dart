9b70cdc68d7beb164776d9cf20bc4adc6fc84a3f
Rydmike <39990307+rydmike@users.noreply.github.com>
2023-08-12 22:30:28 +0300

    ADD: GitHub Actions CI/CD for web build and publish

--- lib/app/constants/app_data.dart
+++ lib/app/constants/app_data.dart
@@ -28,8 +28,8 @@ class AppData {
   // Version of the WEB build, usually same as package, but it also has a
   // build numbers.
   static const String versionMajor = '1';
-  static const String versionMinor = '2';
-  static const String versionPatch = '1';
+  static const String versionMinor = '3';
+  static const String versionPatch = '0';
   static const String versionBuild = '01';
   static const String version = '$versionMajor.$versionMinor.$versionPatch '
       'Build-$versionBuild';
